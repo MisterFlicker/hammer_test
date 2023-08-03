d9fb9dee48a909740160cd98145ecc0520e8cde9
Andrey <lutecequantum@gmail.com>
2023-08-04 00:19:26 +0500

    allow all urls

--- hammer_test/settings.py
+++ hammer_test/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,11 +25,8 @@ SECRET_KEY = 'django-insecure-cbxxo7o$53iu89=_-mvgs8j+_z1w%f^!g-l09s&j@spz&jl8g#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
