e72eeaa0149fd89f3d81a37f890e451b4098e2fe
Andrey <lutecequantum@gmail.com>
2023-08-04 02:02:19 +0500

    come first length model

--- hammer_test/authorization/models.py
+++ hammer_test/authorization/models.py
@@ -4,13 +4,13 @@ from django.core.validators import RegexValidator
 
 class Phones_n_codes(models.Model):
     phone = models.CharField(
-        'phone',
         max_length=11,
         validators=[RegexValidator(regex='^8[0-9]{10}$', message='Incorrect phone')],
+        'phone',
     )
     self_code = models.CharField(max_length=6)
     entered_code = models.CharField(
-        'entered_code',
         max_length=6,
         validators=[RegexValidator(regex='^[0-9a-zA-Z]{6}$', message='Incorrect code')],
+        'entered_code',
     )
