d78afa4a46eb11794464a36421a53de973eb5d09
Andrey <lutecequantum@gmail.com>
2023-08-03 19:11:31 +0500

    max_length for Phones_n_codes edited

--- hammer_test/authorization/models.py
+++ hammer_test/authorization/models.py
@@ -6,9 +6,11 @@ class Phones_n_codes(models.Model):
     phone = models.CharField(
         'phone',
         validators=[RegexValidator(regex='^8[0-9]{10}$', message='Incorrect phone')],
+        max_length=11,
     )
     self_code = models.CharField(max_length=6)
     entered_code = models.CharField(
         'entered_code',
         validators=[RegexValidator(regex='^[0-9a-zA-Z]{6}$', message='Incorrect code')],
+        max_length=6,
     )
