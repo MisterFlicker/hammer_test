d2692e41081badbcdba508bd96219ab30d98b334
Andrey <lutecequantum@gmail.com>
2023-08-04 16:03:57 +0500

    las change back

--- hammer_test/authorization/models.py
+++ hammer_test/authorization/models.py
@@ -3,6 +3,12 @@ from django.core.validators import RegexValidator
 
 
 class Phones_n_codes(models.Model):
-    phone = models.CharField(max_length=11)
+    phone = models.CharField(
+        max_length=11,
+        validators=[RegexValidator(regex='^8[0-9]{10}$', message='Incorrect phone')],
+    )
     self_code = models.CharField(max_length=6)
-    entered_code = models.CharField(max_length=6)
+    entered_code = models.CharField(
+        max_length=6,
+        validators=[RegexValidator(regex='^[0-9a-zA-Z]{6}$', message='Incorrect code')],
+    )
